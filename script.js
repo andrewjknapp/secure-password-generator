160233517b77f59433858defc9550c5dc375fbba
Armadillo <ajknapp2015@gmail.com>
2020-01-21 12:53:46 -0800

    tested code for functionality. only generates the phrase password

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Generates secure password when called
+function generatePassword() {
+  return "password";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  
 
 }
 
