d347efa07bfa34cf9dc25160135d17e3daa7953d
Armadillo <ajknapp2015@gmail.com>
2020-01-21 14:02:41 -0800

    Created function getCharacterSelection which prompts user for which character types they want in their password and added this to the generatePassword function to produce a password with given characters.

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 //Array of digits
-let digits = [0,1,2,3,4,5,6,7,8,9];
+let digits = ['0','1','2','3','4','5','6','7','8','9'];
 
 //Array of lowercase letters
-let lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+let lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 //Array of uppercase letters
-let uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+let uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
-//
+//Array of special characters
+let special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
 
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+
+//Prompts user to determine which characters the user wants
+//Returns an array with name of each character wanted
+function getCharacterSelection() {
+
+  let characterArray = [];
+  let digitCheck = confirm("Include Numerical Digits in Secure Password?");
+  let lowerCheck = confirm("Include Lowercase Letters in Secure Password?");
+  let upperCheck = confirm("Include Uppercase Letters in Secure Password?");
+  let specialCheck = confirm("Include Special Characters in Secure Password?");
+
+  if (digitCheck) {
+    characterArray.push(...digits);
+  }
+
+  if (lowerCheck) {
+    characterArray.push(...lowercase);
+  }
+
+  if (upperCheck) {
+    characterArray.push(...uppercase);
+  }
+
+  if (specialCheck) {
+    characterArray.push(...special);
+  }
+  
+  return characterArray;
+}
 
 //Generates secure password when called
 function generatePassword() {
-  return "password";
+
+  let passLength = parseInt(prompt("Please enter the length of desired password as an integer from 8 to 128"));
+  
+  let charSelect = getCharacterSelection();
+  let password = "";
+
+  for (let i=0; i<passLength; i++) {
+    password += getRandom(charSelect);
+  }
+  return password;
 }
 
 //Returns value of random index of an array
@@ -25,13 +62,15 @@ function getRandom(arr) {
   return randElement;
 }
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
 
 }
 
