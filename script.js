4f218b1e3d180b3ecb021807c6e27427383089e3
Armadillo <ajknapp2015@gmail.com>
2020-01-21 13:07:32 -0800

    Added arrays of lowercase letters, uppercase letters, and digits

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//Array of digits
+let digits = [0,1,2,3,4,5,6,7,8,9];
+
+//Array of lowercase letters
+let lowercase = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+//Array of uppercase letters
+let uppercase = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z];
+
+//
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -6,6 +18,12 @@ function generatePassword() {
   return "password";
 }
 
+//Returns value of random index of an array
+function getRandom(arr) {
+  var randIndex = Math.floor(Math.random() * arr.length);
+  var randElement = arr[randIndex];
+  return randElement;
+}
 
 // Write password to the #password input
 function writePassword() {
