fece92da58cce3173380fa1ae6aa1dd000699fcd
Armadillo <ajknapp2015@gmail.com>
2020-01-21 16:28:58 -0800

    Used regular expressions to check generated password to ensure it meets criteria when all character types are chosen

--- script.js
+++ script.js
@@ -1,44 +1,49 @@
 //Array of digits
 let digits = ['0','1','2','3','4','5','6','7','8','9'];
+let digitRegex = /[0-9]/;
 
 //Array of lowercase letters
 let lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+let lowerRegex = /[a-z]/;
 
 //Array of uppercase letters
 let uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let upperRegex = /[A-Z]/;
 
 //Array of special characters
 let special = ['!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-
+let specialRegex = /[!"#$%&()*+,-./:;<=>?@[\\\]\^_`{\|}~]/;
 
 
 //Prompts user to determine which characters the user wants
-//Returns an array with name of each character wanted
+//Returns an array with an array of characters wanted and
+// an array of all characters to choose from.
 function getCharacterSelection() {
 
-  let characterArray = [];
+  
+  let allCharacterArray = [];
   let digitCheck = confirm("Include Numerical Digits in Secure Password?");
   let lowerCheck = confirm("Include Lowercase Letters in Secure Password?");
   let upperCheck = confirm("Include Uppercase Letters in Secure Password?");
   let specialCheck = confirm("Include Special Characters in Secure Password?");
 
   if (digitCheck) {
-    characterArray.push(...digits);
+    allCharacterArray.push(...digits);
   }
 
   if (lowerCheck) {
-    characterArray.push(...lowercase);
+    allCharacterArray.push(...lowercase);
   }
 
   if (upperCheck) {
-    characterArray.push(...uppercase);
+    allCharacterArray.push(...uppercase);
   }
 
   if (specialCheck) {
-    characterArray.push(...special);
+    allCharacterArray.push(...special);
   }
   
-  return characterArray;
+  return allCharacterArray;
 }
 
 //Generates secure password when called
@@ -57,12 +62,24 @@ function generatePassword() {
 
   } while(!lengthCheck);
   
+
+
   let charSelect = getCharacterSelection();
   let password = "";
 
-  for (let i=0; i<passLength; i++) {
-    password += getRandom(charSelect);
-  }
+  do {
+
+    for (let i=0; i<passLength; i++) {
+      password += getRandom(charSelect);
+    }
+    
+    
+  } while(
+    !(digitRegex.test(password) &&
+    lowerRegex.test(password) &&
+    upperRegex.test(password) &&
+    specialRegex.test(password))
+  )
   return password;
 }
 
